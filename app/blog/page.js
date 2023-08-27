f98fa44e7e113a3553a6de1eb568eb51f970290f
Eli Filiba <efiliba@hotmail.com>
2023-08-27 10:00:53 +0300

    Add Neon helloWorld

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,13 @@
+import { helloWorld } from "@/lib/db";
 
+export default async function BlogPage() {
+  const { data, latency } = await helloWorld();
 
-export default function BlogPage () {
-   return <main>
-    <h1>New Blog Page</h1>
-   </main>
+  return <main>
+    <h1>New Blog Page {Date.now()}</h1>
+    <pre>{JSON.stringify({ data, latency }, null, 2)}</pre>
+  </main>
 }
+
+export const runtime = 'edge';
+export const preferredRegion = 'sin1';
