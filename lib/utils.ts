66384050245a028c97d06df44b835f4990a7a168
Eli Filiba <efiliba@hotmail.com>
2023-08-27 12:37:11 +0300

    Test NEXT_PUBLIC_VERCEL_BRANCH_URL

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,13 +6,15 @@ export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 export const getDomain = () => {
   console.log("getDomain:");
   console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
-  console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV);
   console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
-  console.log("process.env.VERCEL_URL", process.env.VERCEL_URL);
-
+  console.log("process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL", process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL);
   console.log("______________________________");
-  
-  return process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production'
+
+  // switch ()
+
+  return process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : 'http://localhost:3000';
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
+      : 'http://localhost:3000';
 }
