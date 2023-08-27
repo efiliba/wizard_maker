ca34dc85832694909ec87b73aae1b194fa87294f
Eli Filiba <efiliba@hotmail.com>
2023-08-27 12:26:35 +0300

    Check env variables

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,16 @@ import { twMerge } from "tailwind-merge";
  
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-export const getDomain = () =>
-  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+export const getDomain = () => {
+  console.log("getDomain:");
+  console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
+  console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV);
+  console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
+  console.log("process.env.VERCEL_URL", process.env.VERCEL_URL);
+
+  console.log("______________________________");
+  
+  return process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production'
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3000';
+}
