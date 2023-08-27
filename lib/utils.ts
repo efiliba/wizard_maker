8f0a6b315cfa4c673aaf0bb0a48b5fedb4f2d80d
Eli Filiba <efiliba@hotmail.com>
2023-08-27 12:52:55 +0300

    Fix getDomain for prod

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,19 +3,8 @@ import { twMerge } from "tailwind-merge";
  
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-export const getDomain = () => {
-  console.log("getDomain:");
-  console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
-  console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
-  console.log("process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL", process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL);
-  console.log("process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG", process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG);
-  console.log("______________________________");
-
-  // switch ()
-
-  return process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}`
+export const getDomain = () => process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}.vercel.app`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
       : 'http://localhost:3000';
-}
