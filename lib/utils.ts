4c407e840263594bee9535cd8174328ff7cbde62
Eli Filiba <efiliba@hotmail.com>
2023-08-27 12:49:22 +0300

    Test NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,12 +8,13 @@ export const getDomain = () => {
   console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
   console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
   console.log("process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL", process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL);
+  console.log("process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG", process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG);
   console.log("______________________________");
 
   // switch ()
 
   return process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
       : 'http://localhost:3000';
