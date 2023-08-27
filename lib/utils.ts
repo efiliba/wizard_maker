097477763c3771f60172bb5d8ca7f4d7f34a5feb
Eli Filiba <efiliba@hotmail.com>
2023-08-27 12:56:00 +0300

    Replace _ with - in getDomain

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 export const getDomain = () => process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}.vercel.app`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG?.replace('_', '-')}.vercel.app`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
       : 'http://localhost:3000';
